e07fb94518a1a50d4b5d64a7988dd0ca5c6b4c3e
Jade <ktay0557@gmail.com>
2025-05-06 17:18:15 +0100

    Adding comments to functions

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ let currentRoundNumber = 1;
 
 // called at the start to generate new secret target number
 function generateTarget() {
-  return Math.floor(Math.random() * 9);
+  return Math.floor(Math.random() * 10);
 };
 
 // function to determine which guess is closest to the secret number
 function compareGuesses(userGuess, computerGuess, secretNumber) {
+  if (userGuess < 0 || userGuess > 9) {
+    alert('Your guess is out of range! Please enter a number between 0 and 9.');
+    return false;
+  };
   const humanDifference = Math.abs(secretNumber - userGuess);
   const computerDifference = Math.abs(secretNumber - computerGuess);
   return humanDifference <= computerDifference;
@@ -23,6 +27,7 @@ function updateScore(winner) {
   };
 };
 
+// updates the round number after each round
 function advanceRound() {
   currentRoundNumber++;
 };
